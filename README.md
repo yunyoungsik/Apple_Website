f14b8fa4bc76dd3487da781cdcb4beb8947ed55a
yunyoungsik <yunyoungsik91@gmail.com>
2024-04-30 18:03:31 +0900

    20240430-완성

--- README.md
+++ README.md
@@ -5,5 +5,8 @@
 ```
 npm create vite@latest ./ -- --template react    
 npm install -D tailwindcss postcss autoprefixer    
-npx tailwindcss init -p 
+npx tailwindcss init -p    
+npm install gsap @gsap/react    
+npm install three @react-three/drei @react-three/fiber    
+npm install --save @sentry/react
 ```
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://37eca8c3e03e7586ecad643b49c5eb56@o4507174363594752.ingest.us.sentry.io/4507174369361920",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
